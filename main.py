3298e90d30c29299d85301ca8f6348fa7816e3ec
Jack Toumey <jack@toumey.io>
2026-01-16 19:16:11 -0700

    using first tool definition

--- main.py
+++ main.py
@@ -1,2 +1,31 @@
+from langchain_openai import ChatOpenAI
+from langchain.prompts import (
+  ChatPromptTemplate,
+  HumanMessagePromptTemplate,
+  MessagesPlaceholder,
+)
+from langchain.agents import OpenAIFunctionsAgent, AgentExecutor
+from tools.sql import run_query_tool
 
-print("Hell world")
+chat = ChatOpenAI()
+prompt = ChatPromptTemplate(
+  messages=[
+    HumanMessagePromptTemplate.from_template("{input}"),
+    MessagesPlaceholder(variable_name="agent_scratchpad"),
+  ]
+)
+tools = [run_query_tool]
+
+agent = OpenAIFunctionsAgent(
+  llm=chat,
+  prompt=prompt,
+  tools=tools,
+)
+
+agent_executor = AgentExecutor(
+  agent=agent,
+  verbose=True,
+  tools=tools,
+)
+
+agent_executor("How many users are in the database?")

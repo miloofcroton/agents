80bf14087222eee28b8abf3b2be6b74f846a05d5
Jack Toumey <jack@toumey.io>
2026-01-16 22:27:37 -0700

    better arg names for llm

--- tools/sql.py
+++ tools/sql.py
@@ -1,5 +1,8 @@
 import sqlite3
+from pydantic.v1 import BaseModel
+from typing import List
 from langchain.tools import Tool
+
 
 conn = sqlite3.connect("db.sqlite")
 
@@ -29,14 +32,25 @@ def describe_tables(table_names):
   return "\n".join(row[0] for row in rows if row[0] is not None)
 
 
+class RunQueryArgsSchema(BaseModel):
+  query: str
+
+
 run_query_tool = Tool.from_function(
   name="run_sqlite_query",
   description="Run a sqlite query",
   func=run_sqlite_query,
+  args_schema=RunQueryArgsSchema,
 )
+
+
+class DescribeTablesArgsSchema(BaseModel):
+  table_names: List[str]
+
 
 describe_tables_tool = Tool.from_function(
   name="describe_tables",
   description="Given a list of table names, returns the schema of those tables",
   func=describe_tables,
+  args_schema=DescribeTablesArgsSchema,
 )

7c6a420f300ac5d0454cdf838fe9cd9c7638db23
Jack Toumey <jack@toumey.io>
2026-01-16 22:24:21 -0700

    better system message so tools get used

--- main.py
+++ main.py
@@ -13,7 +13,12 @@ chat = ChatOpenAI()
 prompt = ChatPromptTemplate(
   messages=[
     SystemMessage(
-      content=f"You are an AI that has access to a SQLite database.\n{tables}"
+      content=(
+        "You are an AI that has access to a SQLite database.\n"
+        f"The database has tables of: {tables}\n"
+        "Do not make any assumptions about what tables exist or what columns exist."
+        "Instead, use the 'describe_tables' function."
+      )
     ),
     HumanMessagePromptTemplate.from_template("{input}"),
     MessagesPlaceholder(variable_name="agent_scratchpad"),
